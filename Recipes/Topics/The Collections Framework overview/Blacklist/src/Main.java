5f3a35029f3e3e896acdbc330a61affb415185e9
Igor Shmagrinskiy <unrealwork@gmail.com>
2021-08-21 10:28:02 +0300

    Initial commit

---
+++ Recipes/Topics/The Collections Framework overview/Blacklist/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class CollectionUtils {
+
+    public static Collection<String> filterPhones(Collection<String> phones,
+                                                  Collection<String> blacklist) {
+        // write your code here
+        HashSet<String> phonesSet = new HashSet<>(phones);
+        phonesSet.removeAll(blacklist);
+        return Collections.unmodifiableSet(phonesSet);
+    }
+}
+
+/* Please, do not modify this I/O code */
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Collection<String> phones = Arrays.asList(scanner.nextLine().split("\\s+"));
+        Collection<String> blockList = Arrays.asList(scanner.nextLine().split("\\s+"));
+
+        Collection<String> nonBlockedPhones = CollectionUtils.filterPhones(
+                new ArrayList<>(phones), new ArrayList<>(blockList));
+
+        System.out.println(nonBlockedPhones.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(" ")));
+    }
+}
