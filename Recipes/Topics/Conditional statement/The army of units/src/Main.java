e0ef32259864e9c7b4b7b41e76ea8c8edb3d96de
Igor Shmagrinskiy <unrealwork@gmail.com>
2021-09-02 07:57:12 +0300

    - problem of the day - stage 3

---
+++ Recipes/Topics/Conditional statement/The army of units/src/Main.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        try (Scanner scanner = new Scanner(System.in)) {
+            
+        }
+    }
+}
