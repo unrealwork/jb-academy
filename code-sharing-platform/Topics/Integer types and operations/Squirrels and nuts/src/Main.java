a7d01e853cc6669d7ad9c09bcaa85c2b30757652
unrealwork <unrealwork@gmail.com>
2021-10-05 08:21:49 +0300

    - problem of the day

---
+++ code-sharing-platform/Topics/Integer types and operations/Squirrels and nuts/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // put your code here
+        int n = scanner.nextInt();
+        int k = scanner.nextInt();
+        int p = k / n;
+        System.out.println(k - p * n);
+    }
+}
