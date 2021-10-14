bafcc490922d3819edf47dfcda1066bbe6422673
unrealwork <unrealwork@gmail.com>
2021-10-14 09:33:49 +0300

    repeat topics

---
+++ file-server/Topics/The Set interface/Removing elements/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class SetUtils {
+
+    public static Set<Integer> getSetFromString(String str) {
+        // write your code here
+    }
+
+    public static void removeAllNumbersGreaterThan10(Set<Integer> set) {
+        // write your code here
+    }
+
+}
+
+/* Do not change code below */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String numbers = scanner.nextLine();
+        Set<Integer> set = SetUtils.getSetFromString(numbers);
+        SetUtils.removeAllNumbersGreaterThan10(set);
+        set.forEach(e -> System.out.print(e + " "));
+    }
+}
