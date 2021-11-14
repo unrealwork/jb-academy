b9b0d203df908c7ab62a6b995021b7098eadccbe
Igor Shmagrinskiy <unrealwork@gmail.com>
2021-11-14 15:05:25 +0300

    - problem of the day

---
+++ shared-expenses-tracker/Topics/Constructor/Book/src/Main.java
@@ -0,0 +1,12 @@
+class Book {
+        public Book(String title, int yearOfPublishing, String[] authors) {
+            this.title = title;
+            this.yearOfPublishing = yearOfPublishing;
+            this.authors = authors;
+        }
+
+        String title;
+        int yearOfPublishing;
+        String[] authors;
+
+    }
