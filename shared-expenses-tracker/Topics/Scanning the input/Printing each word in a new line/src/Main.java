e03ff74008e960c4cca41d2d8167df6fe7677e8f
Igor Shmagrinskiy <unrealwork@gmail.com>
2021-11-15 15:57:16 +0300

    - problem of the day

---
+++ shared-expenses-tracker/Topics/Scanning the input/Printing each word in a new line/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+@SuppressWarnings("squid:S106")
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(sc.next());
+        }
+    }
+}
