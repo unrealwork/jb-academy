8db87b610aadc6c255d965be0b465a40f75954ec
unrealwork <unrealwork@gmail.com>
2022-04-17 13:14:05 +0300

    recursion

--- Numeric Matrix Processor/Topics/Recursion/Number of decompositions/src/Main.java
+++ Numeric Matrix Processor/Topics/Recursion/Number of decompositions/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            dec(n, n, "");
+        }
+    }
+
+    public static void dec(int n, int max, String str) {
+        if (n == 0) {
+            System.out.println(str);
+        } else {
+            if (max > 1) {
+                dec(n, max - 1, str);
+            }
+            if (max <= n) {
+                dec(n - max, max, str + " " + max);
+            }
+        }
     }
 }
